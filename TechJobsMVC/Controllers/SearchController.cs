d0e2660ebb81e0ce06476fd0a6765b77dde17001
aloepker <91484770+aloepker@users.noreply.github.com>
2022-01-12 08:39:10 -0600

    TODO 3 is confusing me so far still

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -18,10 +18,16 @@ namespace TechJobsMVC.Controllers
         }
 
         // TODO #3: Create an action method to process a search request and render the updated search view. 
-        public IActionResult Results()
-        {
-            return View();
+       //2 The Results method should take in two parameters, specifying the type of search and the search term.
+       // 3 In order for these parameters to be properly passed in by ASP.NET, you need to name them appropriately, based on the corresponding form field names defined in Search/Index.
+       // 4 If the user leaves the search box empty, call the FindAll() method from JobData. Otherwise, send the search information to FindByColumnAndValue. In either case, store the results in a jobs list.
+      // 5 Pass jobs into the Search/Index view via ViewBag.
+      // 6 Pass ListController.ColumnChoices into the view, as the existing search handler does
+        public IActionResult Results(string searchType, string searchTerm)
+       {
+            return View(ListController.ColumnChoices);//Content("<p1>"+searchTerm + " & " + searchType + "<p1/>","text,html");   // no results view exists it says tho, so probably something else here
         }
         //this is likely where the a1 custom searchmethods go like gerard was talking about
+        //probably adding something to viewbag.jobs so the view can display the search results
     }
 }
